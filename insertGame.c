8ab6cdd8960612ada9c3f43342318bea8187c5d9
Davey Winter <rdwinter3@gmail.com>
2019-04-22 18:38:21 -0500

    memento

--- insertGame.c
+++ insertGame.c
@@ -43,22 +43,36 @@ int main(int argc, char** argv) {
 
   // TODO: 1. Define string variables to hold your game's name, publisher,
   // platform
-
+	char name[256] = "Counter Strike Source";
+	char publisher[256] = "Valve";
+	char platform[256] = "PC";
+	int year = 2004;
   // TODO: 2. Check that your game does not already exist, quit if it does
-
+	
+	VideoGame* game = getGame(name, sqlstatementhandle);
+	if(game!=NULL){
+		exit(0);
+	}
   // TODO: 3. Check that a record for your publisher doesn't already exist,
   //         if it does use it, otherwise add it
-
+		
+	Platform* platformz = getPlatform(platform, sqlstatementhandle);
+  if(platformz==NULL){
+		addPlatform(name, sqlstatementhandle);
+	}
   // TODO: 4. Check that a record for your platform doesn't already exist,
   //         if it does use it, otherwise add it
-
+	Publisher* publisherz = getPublisher(publisher, sqlstatementhandle);
+	 if(platformz==NULL){
+		addPublisher(name, sqlstatementhandle);
+	}
   // TODO: 5. Add the video game to the database
-
+	addVideoGame(name, publisherz.id, sqlstatementhandle);
   // TODO: 6. Load the game from the database
-
+	game = getGame(name, sqlstatementhandle);
   // TODO: 7. Associate your game with the platform with a particular release
   // year
-
+	addAvailability(game.id, platformz.id, year, sqlstatementhandle);
   // Cleans up connection before exiting
   finishConnection(sqlenvhandle, sqlstatementhandle, sqlconnectionhandle);
 }
